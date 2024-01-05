ad6ec78c70c9fa69e4e63fc5b724eb4e91a9a648
Rustam Gilmutdinov <rukavina_sb@mail.ru>
2024-01-05 19:35:23 +0500

    refactor(basic 3.1): rename section tag 'incompletedTasks' into 'main__incomplete-tasks' according to BEM rules

--- app.js
+++ app.js
@@ -10,7 +10,7 @@
 
 var taskInput=document.getElementById("main__new-task");//Add a new task.
 var addButton=document.getElementsByTagName("button")[0];//first button
-var incompleteTaskHolder=document.getElementById("incompleteTasks");//ul of #incompleteTasks
+var incompleteTaskHolder=document.getElementById("main__incomplete-tasks");//ul of #incompleteTasks
 var completedTasksHolder=document.getElementById("completed-tasks");//completed-tasks
 
 
